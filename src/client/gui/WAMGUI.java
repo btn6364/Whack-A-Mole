c03fb6457a98d1015d0f8f3fe9e1b136c1355c80
jms8376 <47234441+jms8376@users.noreply.github.com>
2019-04-10 23:33:35 -0400

    Second commit.
    I have started the GUI and now the server

--- src/client/gui/WAMGUI.java
+++ src/client/gui/WAMGUI.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 /**
  * @author Jared Sullivan (jms8376@rit.edu)
+ * @author Bao Nguyen (btn6364@rit.edu)
  */
 public class WAMGUI extends Application //implements Observer<type>
 {
