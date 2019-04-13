d843ee2ed47f55beb1241ee33cbe8a631545119e
jms8376 <47234441+jms8376@users.noreply.github.com>
2019-04-13 18:22:51 -0400

    Fourth commit

--- src/server/WAM.java
+++ src/server/WAM.java
@@ -1,9 +1,6 @@
 package server;
 
 public class WAM {
-    public final static int ROWS = 6;
-    public final static int COLS = 7;
-
     private int rows;
     private int cols;
     private Case[][] board;
--- src/server/WAMGame.java
+++ src/server/WAMGame.java
@@ -1,4 +1,24 @@
 package server;
 
-public class WAMGame {
+public class WAMGame implements Runnable{
+
+    private WAMPlayer playerOne;
+    private WAMPlayer playerTwo;
+    private WAMPlayer playerThree;
+    private WAM game; //TODO
+
+    public WAMGame(WAMPlayer playerOne, WAMPlayer playerTwo,
+                   WAMPlayer playerThree)
+    {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+        this.playerThree = playerThree;
+
+    }
+
+    @Override
+    public void run()
+    {
+
+    }
 }
