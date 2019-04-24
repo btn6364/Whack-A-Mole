d328d71ca09fc5fe57820e97163e0e93b8bd59ce
jms8376 <47234441+jms8376@users.noreply.github.com>
2019-04-23 21:38:09 -0400

    More MoleThread work

--- src/server/MoleThread.java
+++ src/server/MoleThread.java
@@ -1,16 +1,36 @@
 package server;
 
+import java.util.Random;
+
 /**
  * @author Jared Sullivan (jms8376@rit.edu)
  */
 public class MoleThread implements Runnable
 {
     int id;
-
     boolean up;
+    WAMGame game;
+
+    public int randomDown() {
+        Random r = new Random();
+        int low = 5;
+        int high = 10;
+        return r.nextInt(high-low) + low;
+    }
+
+    public int randomUp(){
+        Random r = new Random();
+        int low = 2;
+        int high = 5;
+        return r.nextInt(high-low) + low;
+    }
+
 
     public void run(){
-
+        while(true)
+        {
+            int randomUp = randomUp();
+        }
     }
 
     public boolean isUp(){
@@ -19,9 +39,10 @@ public class MoleThread implements Runnable
 
     public void setDown(){
         this.up = false;
+
     }
 
     public void setUp(){
-        this.up = false;
+        this.up = true;
     }
 }
