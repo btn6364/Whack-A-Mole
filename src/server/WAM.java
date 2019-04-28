7d8f8ff4542a0fbd6365d2cd42a3548f2de8de6b
jms8376 <47234441+jms8376@users.noreply.github.com>
2019-04-27 22:17:08 -0400

    fixed errors

--- src/server/WAM.java
+++ src/server/WAM.java
@@ -11,16 +11,15 @@ public class WAM {
     private int[][] board;
     private static final int UP = 1;
     private static final int DOWN = 0;
-    private boolean canRandomizeUp;
-    private boolean canRandomizeDown;
+    private boolean canRandomize;
 
 
     public WAM(int rows, int cols, int timeInSeconds) {
         this.rows = rows;
         this.cols = cols;
         this.timeInSeconds = timeInSeconds;
-        this.canRandomizeUp = true;
-        this.canRandomizeDown = true;
+        this.canRandomize = true;
+
         board = new int[rows][cols];
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
@@ -42,14 +41,24 @@ public class WAM {
         return (board[moleNumber / cols][moleNumber % cols] == UP);
     }
 
-    public int randomizeUp() {
-        if (!canRandomizeUp) {
-            System.out.println();
+    public int getRows()
+    {
+        return rows;
+    }
+
+    public int getCols()
+    {
+        return cols;
+    }
+
+    public int randomize() {
+        if (!canRandomize) {
+            System.out.println("CANNOT randomize");
             return -1;
         }
 
         Random random = new Random();
-        int moleNumber = random.nextInt(rows * cols);
+        int moleNumber = random.nextInt(rows*cols);
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 board[row][col] = DOWN;
@@ -57,48 +66,21 @@ public class WAM {
         }
         setUp(moleNumber);
 
-        int delayInSec = random.nextInt(3) + 3; // from 3 to 5
+        Random r = new Random();
+        int low = 2;
+        int high = 5;
+        int delayInSec = r.nextInt(high-low) + low;
+
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                canRandomizeUp = true;
+                canRandomize = true;
             }
         }, delayInSec * 1000);
-        canRandomizeUp = false;
+        canRandomize = false;
 
         return moleNumber;
     }
-
-    public int randomizeDown(){
-        if (!canRandomizeDown){
-            System.out.println();
-            return -1;
-        }
-
-        Random random = new Random();
-        int moleNumber = random.nextInt(rows * cols);
-        for (int row = 0; row < rows; row++){
-            for (int col = 0; col < cols; col++){
-                board[row][col] = UP;
-            }
-        }
-        setDown(moleNumber);
-
-        int delayInSec = random.nextInt(9) + 2; // from 3 to 5
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                canRandomizeDown = true;
-            }
-        }, delayInSec * 1000);
-        canRandomizeDown = false;
-
-        return moleNumber;
-
-    }
-
-
 
 }
--- src/server/WAMGame.java
+++ src/server/WAMGame.java
@@ -1,35 +1,45 @@
 package server;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class WAMGame implements Runnable {
     private WAMPlayer[] players;
     private WAM wam;
+    private ArrayList<MoleThread> moleThreads = new ArrayList<>();
+    private int time;
+//    private Thread[] threads; not necessary
 
     public WAMGame(WAMPlayer[] players, int cols, int rows, int timeInSeconds) {
         this.wam = new WAM(rows, cols, timeInSeconds);
         this.players = players;
+        this.time = timeInSeconds;
+//        this.threads = new Thread[players.length]; not necessary
     }
 
     @Override
-    public void run() {
-        boolean go = true;
-        while (go) {
-            int randomMoleNumber = wam.randomizeUp();
-            if (randomMoleNumber >= 0) {
-                for (WAMPlayer player : players) {
-                    player.moleUp(randomMoleNumber);
-                }
-            }
-
-            int randomMoleNumberDown = wam.randomizeDown();
-            if (randomMoleNumberDown >= 0){
-                for (WAMPlayer player: players){
-                    player.moleDown(randomMoleNumberDown);
-                }
-            }
-            // check game.hasWon().....
+    public synchronized void run() {
+//        for (int i = 0; i < threads.length; i++) {
+//            threads[i] = new Thread(players[i]);
+//        } not necessary since created the object without running it.
+        Long startingTime = System.currentTimeMillis();
+        int size = wam.getCols() * wam.getRows();
+        for(int i = 0; i < size; i++)
+        {
+            MoleThread moleThread = new MoleThread(i);
+            moleThreads.add(moleThread);
         }
-        for (WAMPlayer player : players) {
-            player.close();
+        for(MoleThread m:moleThreads)
+        {
+            m.run();
+        }
+        while (System.currentTimeMillis() - startingTime < time * 1000) {
+            int randomMoleNumber = wam.randomize();
+
+            // check game.hasWon().....
+//
+//        for (WAMPlayer player : players) {
+//            player.close();
         }
     }
 
@@ -55,4 +65,49 @@ public class WAMGame implements Runnable {
             player.moleDown(moleNumber);
         }
     }
+
+    public int randomUp(){
+        Random r = new Random();
+        int low = 2;
+        int high = 5;
+        return r.nextInt(high-low) + low;
+    }
+
+    public int randomDown() {
+        Random r = new Random();
+        int low = 5;
+        int high = 10;
+        return r.nextInt(high-low) + low;
+    }
+
+    public class MoleThread implements Runnable
+    {
+        private int moleNum;
+
+        public MoleThread(int moleNum)
+        {
+            this.moleNum = moleNum;
+        }
+
+        @Override
+        public void run()
+        {
+            while (true)
+            {
+                try
+                {
+                    setUp(moleNum);
+
+                    // this.wait(randomUp() * 1000);
+                    setDown(moleNum);
+                    // this.wait(randomDown() * 1000);
+
+                } catch (Exception ie)
+                {
+                    System.err.println();
+                }
+            }
+        }
+    }
+
 }
